64c63ebdb3fad7a2bc6b7cf6effb68612df349bf
Simonas Giniunas <simoas.giniunas1@gmailc.com>
2024-04-04 19:22:38 +0300

    Refactor code

---
+++ src/driver.rs
@@ -0,0 +1,158 @@
+use alloc::borrow::ToOwned;
+use alloc::vec;
+use alloc::vec::Vec;
+use cortex_m::delay::Delay;
+use cortex_m::peripheral::SYST;
+use cortex_m::prelude::_embedded_hal_blocking_spi_Transfer;
+use fugit::HertzU32;
+use rtt_target::rprintln;
+use stm32f1xx_hal::afio::MAPR;
+use stm32f1xx_hal::device::SPI1;
+use stm32f1xx_hal::gpio::{Alternate, Output, Pin};
+use stm32f1xx_hal::rcc::Clocks;
+use stm32f1xx_hal::spi::{Mode, Phase, Polarity, Spi, Spi1NoRemap};
+use stm32f1xx_hal::timer::{SysDelay, Timer};
+use crate::constants::{INIT_DELAY, REG_POWER_UP_RESET, READ_ADDRESS_DATA_DELAY, REG_MOTION, REG_DELTA_X_H, REG_DELTA_X_L, REG_DELTA_Y_L, REG_DELTA_Y_H, REG_SROM_ENABLE, SROM_ENABLE_DELAY, REG_SROM_LOAD_BURST, PMW_3360_FIRMWARE, SROM_DOWNLOAD_DELAY, REG_CONFIG_2, REG_MOTION_BURST};
+use crate::motion_data::MotionData;
+
+pub type PmwCe = Pin<'A', 4, Output>;
+pub type PmwSck = Pin<'A', 5, Alternate>;
+pub type PmwMiso = Pin<'A', 6>;
+pub type PmwMosi = Pin<'A', 7, Alternate>;
+pub type PmwSpi = Spi<SPI1, Spi1NoRemap, (PmwSck, PmwMiso, PmwMosi), u8>;
+
+pub struct Driver {
+    spi: PmwSpi,
+    chip_enable_pin: PmwCe,
+    delay: SysDelay,
+}
+
+impl Driver {
+    pub fn new(
+        pmw_ce: PmwCe,
+        pmw_sck: PmwSck,
+        pmw_miso: PmwMiso,
+        pmw_mosi: PmwMosi,
+        spi1: SPI1,
+        mapr: &mut MAPR,
+        syst: SYST,
+        clocks: Clocks,
+    ) -> Self {
+        let pins = (pmw_sck, pmw_miso, pmw_mosi);
+
+        let spi_mode = Mode {
+            polarity: Polarity::IdleHigh,
+            phase: Phase::CaptureOnSecondTransition,
+        };
+
+        let spi = Spi::spi1(spi1, pins, mapr, spi_mode, HertzU32::kHz(10), clocks);
+
+        let delay = Timer::syst(syst, &clocks).delay();
+
+        Self {
+            spi,
+            chip_enable_pin: pmw_ce,
+            delay,
+        }
+    }
+
+    pub fn init(&mut self) {
+        self.chip_enable_pin.set_low();
+        self.delay.delay(INIT_DELAY);
+        self.chip_enable_pin.set_high();
+
+        self.pmw_write(
+            REG_POWER_UP_RESET,
+            vec![0x5a]
+        );
+
+        self.delay.delay(INIT_DELAY);
+        self.pmw_read(REG_MOTION, 1);
+        self.pmw_read(REG_DELTA_X_L, 1);
+        self.pmw_read(REG_DELTA_X_H, 1);
+        self.pmw_read(REG_DELTA_Y_L, 1);
+        self.pmw_read(REG_DELTA_Y_H, 1);
+
+        self.disable_rest_mode();
+
+        self.pmw_write(REG_SROM_ENABLE, vec![0x1d]);
+        self.delay.delay(SROM_ENABLE_DELAY);
+        self.pmw_write(REG_SROM_ENABLE, vec![0x18]);
+        self.pmw_write(REG_SROM_LOAD_BURST, PMW_3360_FIRMWARE.to_vec());
+        self.delay.delay(SROM_DOWNLOAD_DELAY);
+        self.pmw_write(REG_CONFIG_2, vec![0x00]);
+    }
+
+    pub fn enter_loop(&mut self, mut motion_handler: impl FnMut(MotionData)) -> ! {
+        self.pmw_write(REG_MOTION_BURST, vec![0xff]);
+        loop {
+            let motion_data: MotionData = self.pmw_read(REG_MOTION_BURST, 12).into();
+            motion_handler(motion_data);
+        }
+    }
+
+    fn enable_rest_mode(
+        &mut self
+    ) {
+        let mut config2 = self.pmw_read(0x10, 1);
+        config2[0] |= 1 << 5;
+        self.pmw_write(0x10, config2);
+    }
+
+    fn disable_rest_mode(
+        &mut self
+    ) {
+        let mut config2 = self.pmw_read(0x10, 1);
+        config2[0] &= !(1 << 5);
+        self.pmw_write(0x10, config2);
+    }
+
+    fn pmw_write(
+        &mut self,
+        address: u8,
+        data: Vec<u8>,
+    ) {
+        self.pmw_transfer(true, address, data);
+    }
+
+    fn pmw_read(
+        &mut self,
+        address: u8,
+        count: usize,
+    ) -> Vec<u8> {
+        self.pmw_transfer(
+            false,
+            address,
+            vec![0xff; count],
+        )
+    }
+
+    fn pmw_transfer(
+        &mut self,
+        is_write: bool,
+        address: u8,
+        mut data: Vec<u8>,
+    ) -> Vec<u8> {
+        let first_byte = if is_write {
+            (1 << 7) | address
+        } else {
+            !(1 << 7) & address
+        };
+
+        self.chip_enable_pin.set_low();
+
+        self.spi.transfer(&mut [first_byte])
+            .expect("Failed to transfer bytes over SPI1.");
+
+        self.delay.delay(READ_ADDRESS_DATA_DELAY);
+
+        let result = self.spi
+            .transfer(&mut data)
+            .expect("Failed to transfer bytes over SPI1.")
+            .to_owned();
+
+        self.chip_enable_pin.set_high();
+
+        result
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,90 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+#![feature(alloc_error_handler)]
+
+pub mod constants;
+pub mod driver;
+pub mod motion_data;
+
+extern crate alloc;
+
+use alloc::borrow::ToOwned;
+use alloc::vec;
+use alloc::vec::Vec;
+use alloc_cortex_m::CortexMHeap;
+use core::alloc::Layout;
+use cortex_m::asm::delay;
+use cortex_m::peripheral::SYST;
+use panic_rtt_target as _;
+
+use crate::constants::{
+    INIT_DELAY, PMW_3360_FIRMWARE, REG_POWER_UP_RESET, READ_ADDRESS_DATA_DELAY, SROM_DOWNLOAD_DELAY,
+    SROM_ENABLE_DELAY,
+};
+use crate::driver::Driver;
+use cortex_m_rt::entry;
+use fugit::{Duration, Hertz, HertzU32, MicrosDurationU32};
+use rtt_target::{rprint, rprintln, rtt_init_print};
+use stm32f1xx_hal::device::SPI1;
+use stm32f1xx_hal::gpio::{Alternate, Output, Pin};
+use stm32f1xx_hal::pac::{CorePeripherals, Peripherals};
+use stm32f1xx_hal::rcc::Clocks;
+use stm32f1xx_hal::spi::Spi1NoRemap;
+use stm32f1xx_hal::time::Hz;
+use stm32f1xx_hal::timer::{SysDelay, Timer};
+use stm32f1xx_hal::{
+    prelude::*,
+    spi::{Mode, Phase, Polarity, Spi},
+};
+
+type PmwSpi =
+    Spi<SPI1, Spi1NoRemap, (Pin<'A', 5, Alternate>, Pin<'A', 6>, Pin<'A', 7, Alternate>), u8>;
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+
+    unsafe {
+        let start = cortex_m_rt::heap_start() as usize;
+        ALLOCATOR.init(start, 16384);
+    }
+
+    let cp = CorePeripherals::take().unwrap();
+    let dp = Peripherals::take().unwrap();
+
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
+    let mut afio = dp.AFIO.constrain();
+    let mut gpioa = dp.GPIOA.split();
+
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+    let mut driver = Driver::new(
+        gpioa.pa4.into_push_pull_output(&mut gpioa.crl),
+        gpioa.pa5.into_alternate_push_pull(&mut gpioa.crl),
+        gpioa.pa6.into_floating_input(&mut gpioa.crl),
+        gpioa.pa7.into_alternate_push_pull(&mut gpioa.crl),
+        dp.SPI1,
+        &mut afio.mapr,
+        cp.SYST,
+        clocks,
+    );
+    driver.init();
+
+    let mut position_x = 0f32;
+    let mut position_y = 0f32;
+
+    driver.enter_loop(|motion_data| {
+        position_x += motion_data.delta_x as f32 / 65536f32;
+        position_y += motion_data.delta_y as f32 / 65536f32;
+
+        rprintln!("{:?} {:?}", position_x, position_y);
+    });
+}
+
+#[global_allocator]
+pub static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
+
+#[alloc_error_handler]
+fn oom(_: Layout) -> ! {
+    panic!("Ran out of memory");
 }
